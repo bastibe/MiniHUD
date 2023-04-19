96f9f6999ba1c48febeefd7cbdb2e6fab1decd18
Bastian Bechtold <bastibe@MacDesktop.fritz.box>
2023-04-19 19:31:27 +0200

    made compress script executable
    and added hashbang

--- compress.sh
+++ compress.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 cd ../
 rm MiniHUD.zip
 zip -r -D MiniHUD.zip MiniHUD/64/* MiniHUD/data/* MiniHUD/liblinux/* MiniHUD/README.md -x \*/.DS_Store \*/readme.txt \*/SASLLog.txt \*/state.txt
